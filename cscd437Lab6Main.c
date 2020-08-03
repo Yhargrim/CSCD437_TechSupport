16a3d9a803d9ef64b727643b316c1e0b930684ab
Yhargrim <58047675+Yhargrim@users.noreply.github.com>
2020-08-02 17:57:18 -0700

    C update: Name/Numbers working

--- cscd437Lab6Main.c
+++ cscd437Lab6Main.c
@@ -1,57 +1,116 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <regex.h>
+#include <limits.h>
+#include <errno.h>
 
-char* name(char*);
+struct Logs {
+	char* first;
+	char* second;
+	char* third;
+	char* fourth;
+};
+
+void logging(struct Logs*, char*);
+static void name(char*);
 void cullNewLine(char*);
-int* numbers();
-int number();
-FILE* openFile(char*);
-char* password();
+static void number(char*);
+static void openFile(char*);
+static void password();
+static void reenterPassword();
+static void writeToOutput();
+void numError(char*, char*);
 
-int main() {
-	char* first = name("first");
-	char* last = name("last");
-	printf("Name: %s %s\n", first, last);
+static struct Logs mErrors; 
+static bool mNameCorrect, mNumberCorrect, mInFileCorrect, mOutFileCorrect, mPasswordCorrect;
+static FILE* mInput, mOutput;
+static char* mFirst;
+static char* mLast;
+static char* mNum1;
+static char* mNum2;
+static char* mPassword;
 
-	int* nums = numbers();
-	printf("Addition: %d\n", nums[0] + nums[1]);
-	printf("Multiply: %d\n", nums[0] * nums[1]);
+void main() {
+	name("first");
+	name("last");
+	printf("Name: %s %s\n", mLast, mFirst);
 
-	FILE* infile = openFile("r");
+	number("first");
+	number("second");
+	/*
+	openFile("r");
 	char ch;
-	while((ch = fgetc(infile)) != EOF) {
+	while((ch = fgetc(mInput)) != EOF) {
 		printf("%c", ch);
 	}
 
-	FILE* outfile = openFile("w");
-	fprintf(outfile, "Testing output file.\n");
+	openFile("w");
+	fprintf(mOutput, "Testing output file.\n");
 
-	char* pswrd = password();
-	printf("Password: %s\n", pswrd);
+	password();
+	printf("Password: %s\n", mPassword);
+	*/
+	printf("Error:\n\t%s\n\t%s\n\t%s\n\t%s\n", mErrors.first, mErrors.second, mErrors.third, mErrors.fourth);
 
-	free(first);
-	free(last);
-	fclose(infile);
-	fclose(outfile);
-	free(pswrd);
-	return 0;
 }
 
-char* name(char* which) {
-	printf("Enter your %s name: ", which);
-	char* hold = malloc(51);
-	bool flag = true;
-	while (flag) {
- 		if(fgets(hold, 51, stdin) != NULL) {
-			flag = false;
+void logging(struct Logs* mLog, char* log) {
+	if(!mLog->first) {
+		mLog->first = malloc(128);
+		strcpy(mLog->first, log);
+	} else if(!mLog->second) {
+		mLog->second = malloc(128);
+		strcpy(mLog->second, log);
+	} else if(!mLog->third) {
+		mLog->third = malloc(128);
+		strcpy(mLog->third, log);
+	} else if(!mLog->fourth) {
+		mLog->fourth = malloc(128);
+		strcpy(mLog->fourth, log);
+	} else
+		printf("Error logs full.\n");
+}
+
+bool regexMatch(char* str, char* pattern) {
+	regex_t regex;
+	int regInt;
+	regInt = regcomp(&regex, pattern, REG_EXTENDED);
+	if(regInt != 0)
+		return false;
+	regInt = regexec(&regex, str, 0, NULL, 0);
+	regfree(&regex);
+	if(regInt == 0)
+		return true;
+	return false;
+}
+
+static void name(char* which) {
+	printf("Enter %s name: ", which);
+	char* name = malloc(51);
+	char* log = malloc(128);
+ 	if(fgets(name, 51, stdin)) {
+		cullNewLine(name);
+		char* pattern = "^[A-Za-z,.'-]{1,50}$";
+		if(regexMatch(name, pattern)) {
+			mNameCorrect = true;
+			if(strcmp(which, "first") == 0) {
+				mFirst = name;
+		 	} else {
+				mLast = name;
+			}
 		} else {
-			printf("Invalid - Try Again: ");
+			asprintf(&log, "Error when reading %s name. Entered bad value '%s'\n", which, name);
+			cullNewLine(log);
+			logging(&mErrors, log);
 		}
+	} else {
+		asprintf(&log, "Error when reading %s name. Entered bad value '%s'\n", which, name);
+		cullNewLine(log);
+		logging(&mErrors, log);
 	}
-	cullNewLine(hold);
-	return hold;
 }
 
 void cullNewLine(char* str) {
@@ -59,36 +118,53 @@ void cullNewLine(char* str) {
 	noline = strchr(str, '\n');
 	if(noline) {
 		*noline = '\0';
+	} else {
+		while(getchar() != '\n');
 	}
 }
 
-int* numbers() {
-	printf("Enter an integer: ");
-	int one = number();
-
-	printf("Enter another integer: ");
-	int two = number();
-
-	static int arr[2];
-	arr[0] = one;
-	arr[1] = two;
-	return arr;
-}
-
-int number() {
-	int hold;
-	bool flag = true;
-	while (flag) {
-		if (scanf("%d", &hold) == 1) {
-			flag = false;
+static void number(char* which) {
+	printf("Enter %s integer: ", which);
+	char* number = malloc(48);
+	if(fgets(number, 48, stdin)) {
+		cullNewLine(number);
+		char* pattern = "^(-?[0-9]+){1,11}$";
+		if(regexMatch(number, pattern)) {
+			char* endptr;
+			long num = strtol(number, &endptr, 10);
+			if(endptr != number) {
+				if(errno == 0) {
+					if(num >= INT_MIN && num <= INT_MAX) {
+						mNumberCorrect = true;
+						if(strcmp(which, "first") == 0) {
+							mNum1 = number;
+						} else {
+							mNum2 = number;
+						}
+					} else {
+						numError(which, number);
+					}
+				} else {
+					numError(which, number);
+				}
+			} else {
+				numError(which, number);
+			}
 		} else {
-			printf("Invalid - Try Again: ");
+			numError(which, number);
 		}
-		while((getchar()) != '\n');
+	} else {
+		numError(which, number);
 	}
-	return hold;
 }
 
+void numError(char* which, char* number) {
+	char* log = malloc(128);
+	asprintf(&log, "Error when reading %s number. Entered bad value '%s'\n", which, number);
+	cullNewLine(log);
+	logging(&mErrors, log);
+}
+/*
 FILE* openFile(char* mode) {
 	printf("Enter input file name: ");
 	char* fname = malloc(128);
@@ -132,3 +208,4 @@ char* password() {
 	free(second);
 	return first;
 }
+*/
